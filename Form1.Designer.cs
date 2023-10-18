d0549567b423f5a2266693133b269bc4c25e7b6b
Vladimir Pimonov <vpimonov@mail.ru>
2023-10-18 23:14:01 +0400

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace pos_show
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_scale = new System.Windows.Forms.Button();
+            this.tb_scale = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.plotter = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tb_port
+            // 
+            this.tb_port.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_port.Location = new System.Drawing.Point(56, 7);
+            this.tb_port.Name = "tb_port";
+            this.tb_port.Size = new System.Drawing.Size(181, 22);
+            this.tb_port.TabIndex = 0;
+            this.tb_port.Text = "COM1:115200:8N1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_scale);
+            this.panel1.Controls.Add(this.tb_scale);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btn_close);
+            this.panel1.Controls.Add(this.btn_open);
+            this.panel1.Controls.Add(this.tb_port);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(656, 39);
+            this.panel1.TabIndex = 2;
+            // 
+            // btn_scale
+            // 
+            this.btn_scale.Location = new System.Drawing.Point(530, 3);
+            this.btn_scale.Name = "btn_scale";
+            this.btn_scale.Size = new System.Drawing.Size(75, 30);
+            this.btn_scale.TabIndex = 7;
+            this.btn_scale.Text = "Set scale";
+            this.btn_scale.UseVisualStyleBackColor = true;
+            this.btn_scale.Click += new System.EventHandler(this.btn_scale_Click);
+            // 
+            // tb_scale
+            // 
+            this.tb_scale.Location = new System.Drawing.Point(473, 8);
+            this.tb_scale.Name = "tb_scale";
+            this.tb_scale.Size = new System.Drawing.Size(51, 22);
+            this.tb_scale.TabIndex = 6;
+            this.tb_scale.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(415, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Scale 1/";
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(324, 3);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 30);
+            this.btn_close.TabIndex = 4;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(243, 3);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(75, 30);
+            this.btn_open.TabIndex = 3;
+            this.btn_open.Text = "Open";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // plotter
+            // 
+            this.plotter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plotter.Location = new System.Drawing.Point(0, 39);
+            this.plotter.Name = "plotter";
+            this.plotter.Size = new System.Drawing.Size(656, 647);
+            this.plotter.TabIndex = 2;
+            this.plotter.Paint += new System.Windows.Forms.PaintEventHandler(this.plotter_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 686);
+            this.Controls.Add(this.plotter);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tb_scale;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Panel plotter;
+        private System.Windows.Forms.Button btn_scale;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pos_show
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+        List<Point> points;
+        object p_lock = new object();
+        float scale_denominator;
+        System.Drawing.Drawing2D.Matrix scale;
+        bool new_data = false;
+        System.Windows.Forms.Timer tmr;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            points = new List<Point>();
+            set_scale();
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    points.Add(new Point(i * 600, i * 630));
+            //    //points.Add(new Point(i, i));
+            //}
+
+            tmr = new System.Windows.Forms.Timer();
+            tmr.Interval = 50;
+            tmr.Tick += try_plot;
+        }
+
+        private void try_plot(object sender, EventArgs e)
+        {
+            if (new_data)
+            {
+                new_data = false;
+                plotter.Refresh();
+            }
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            var connection = tb_port.Text.Split(new char[] { ':' });
+
+            if (connection.Length != 3)
+            {
+                MessageBox.Show("Port string error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int baud = 0;
+            if (!int.TryParse(connection[1], out baud))
+            {
+                MessageBox.Show("Port string: baud rate parse error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (connection[2].Length != 3)
+            {
+                MessageBox.Show("Port string: bits description length <> 3", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int data_bits=0;
+            if (connection[2][0] == '7')
+                data_bits = 7;
+            else if (connection[2][0] == '8')
+                data_bits = 8;
+            else if (connection[2][0] == '9')
+                data_bits = 9;
+            else
+            {
+                MessageBox.Show("Port string: data bits  <> 7, 8 or 9", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Parity par = Parity.None;
+            if (connection[2][1] == 'E')
+                par = Parity.Even;
+            else if (connection[2][1] == 'M')
+                par = Parity.Mark;
+            else if (connection[2][1] == 'N')
+                par = Parity.None;
+            else if (connection[2][1] == 'O')
+                par = Parity.Odd;
+            else if (connection[2][1] == 'S')
+                par = Parity.Space;
+            else
+            {
+                MessageBox.Show("Port string: parity <> E,M,N,O,S for Even,Mark,None,Odd,Space", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            StopBits stop_bits = StopBits.One;
+            if (connection[2][2] == '1')
+                stop_bits = StopBits.One;
+            else if (connection[2][2] == '2')
+                stop_bits = StopBits.OnePointFive;
+            else if (connection[2][2] == '3')
+                stop_bits = StopBits.OnePointFive;
+            else
+            {
+                MessageBox.Show("Port string: stop bits  <> 1, 2, 3 for 1, 2, 1.5", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (port != null && port.IsOpen)
+                port.Close();
+
+            try
+            {
+                port = new SerialPort(connection[0], baud, par, data_bits, stop_bits);
+                port.Open();
+                port.NewLine = "\n";
+                btn_open.Enabled = false;
+                btn_close.Enabled = true;
+                ThreadPool.QueueUserWorkItem(reader);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                port = null;
+            }
+
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            if (port != null && port.IsOpen)
+                port.Close();
+
+            btn_close.Enabled = false;
+        }
+
+        private void reader(object state)
+        {
+            tmr.Start();
+
+            var splitter = new char[] { ',' };
+            while (port != null && port.IsOpen)
+            {
+                try
+                {
+                    var str = port.ReadLine();
+                    var items = str.Split(splitter, StringSplitOptions.None);
+
+                    var cnt = items.Length / 2;
+
+                    int x, y;
+                    var pnts = new List<Point>(cnt);
+                    for (int i = 0; i < cnt; i++)
+                    {
+                        if (int.TryParse(items[i * 2], out x) && int.TryParse(items[i * 2 + 1], out y))
+                            pnts.Add(new Point(x, y));
+                        else
+                            Log(string.Format("Error convert {0} pair to point: {1}, {2}", i, items[i * 2], items[i * 2 + 1]));
+                    }
+                    lock (p_lock)
+                    {
+                        points = pnts;
+                    }
+                    new_data = true;
+                }
+                catch (Exception ex)
+                {
+                    break;
+                }
+            }
+
+            btn_open.Enabled = true;
+
+            tmr.Stop();
+        }
+
+        private void Log(string msg)
+        {
+        }
+
+        private void btn_scale_Click(object sender, EventArgs e)
+        {
+            set_scale();
+        }
+
+        private void set_scale()
+        {
+            if (float.TryParse(tb_scale.Text, out scale_denominator))
+            {
+                scale = new System.Drawing.Drawing2D.Matrix();
+                scale.Scale(1.0f / scale_denominator, 1.0f / scale_denominator);
+            }
+        }
+
+        private void plotter_Paint(object sender, PaintEventArgs e)
+        {
+            using (var gfx = plotter.CreateGraphics())
+            {
+                gfx.Clear(Color.White);
+                lock (p_lock)
+                {
+                    foreach (var p in points)
+                    {
+                        gfx.FillRectangle(Brushes.Black, p.X / scale_denominator, plotter.Height - p.Y / scale_denominator, 1, 1);
+                    }
+                }
+            }
+        }
+    }
+}
